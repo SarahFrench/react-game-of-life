2b8bb4c9036da7cb1788d260f5790a48271c2632
Sarah French <sarah@sarahfrench.co.uk>
2020-06-01 00:05:59 +0100

    Fix test name

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('displays the buttons for turns and resetting game', () => {
   const { getByText } = render(<App />);
   const turnButton = screen.getByTestId('take-turn');
   const seedButton = screen.getByTestId('seed-life');
